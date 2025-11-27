92e59ce8742fe2c85c4d1861821b93dbf28fe609
shakeb03 <shakeb0308@gmail.com>
2025-11-27 18:47:56 -0500

    Initial Commit

---
+++ Frenco-Chat/Frenco_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frenco_ChatApp.swift
+//  Frenco-Chat
+//
+//  Created by Shakeb . on 2025-11-27.
+//
+
+import SwiftUI
+
+@main
+struct Frenco_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
